9769337143b2c7e50954458f275c4f81c6a36981
GuascaC <109239941+GuascaC@users.noreply.github.com>
2022-08-17 19:00:41 -0500

    Add files via upload

---
+++ ejercicio3.js
@@ -0,0 +1,26 @@
+'use strict'
+//Realizar un programa que cuente la cantidad de números positivos, negativos y 0
+
+let cannum=0;
+let canpos=0;
+let canneg=0;
+let cancer=0;
+let num=0;
+cannum=prompt("Introduzca la cantidad");
+for(let i=0;i<cannum; i++){
+    num=prompt("Introduzca un número");
+    if (num == 0){
+    let cancer =++1 }
+    else{
+        if (num > 0){
+        let canpos =++1 }
+        else{
+        let canneg =++1 }
+    }
+}
+console.log("Positivos:"+canpos);
+alert("Positivos:"+canpos);
+console.log("Negativos:"+canneg);
+alert("Negativos:"+canneg);
+console.log("Ceros:"+cancer);
+alert("Ceros:"+cancer);
